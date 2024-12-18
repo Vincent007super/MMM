6cec06d8241a6ea0bac1dcdb4882ed50402896c9
89329 <89329@glr.nl>
2024-12-18 16:53:50 +0100

    last update

--- home.php
+++ home.php
@@ -1,4 +1,5 @@
 <?php
 
 include 'views/home_view.php';
+
 ?>
--- views/home_view.php
+++ views/home_view.php
@@ -1,12 +1,49 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>MMM</title>
-  <link rel="stylesheet" href="styles/styles.css">
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Home Page</title>
+    <link rel="stylesheet" href="media/styles/site.css">
+    <link rel="stylesheet" href="media/styles/home.css">  
 </head>
 <body>
- <h1>dd</h1>
+
+<?php include 'header.php' ?>
+
+    <div class="container">
+        <section class="section">
+            <div class="circle"></div>
+            <div class="bar"></div>
+            <div class="small-box-row">
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+            </div>
+            <div class="large-box-row">
+                <div class="large-box"></div>
+                <div class="large-box"></div>
+                <div class="large-box"></div>
+                <div class="large-box"></div>
+            </div>
+        </section>
+
+        <section class="section">
+            <div class="circle"></div>
+            <div class="bar"></div>
+            <div class="small-box-row">
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+                <div class="small-box"></div>
+            </div>
+        </section>
+    </div>
 </body>
 </html>
