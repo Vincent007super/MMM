a5158ce20599de29ec6b01aef848c80bf9f84f5f
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2024-12-18 16:01:29 +0100

    Added standard header
    - header.php File can be added to every page and should work on it's own
    - site.css has header css and should be added to each page individually

---
+++ header.php
@@ -0,0 +1,7 @@
+<header>
+    <div id="layer1" class="layer1_inactive"></div>
+    <div id="layer2"></div>
+    <h1 onclick="header()">&#9835;</h1>
+</header>
+
+<script src="code/header.js"></scrip >
