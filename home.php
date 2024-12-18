dc7cc47f5c7b038976aa9034f4a4b2f5726d235f
89329 <89329@glr.nl>
2024-12-18 14:51:28 +0100

    eerste commit

---
+++ home.php
@@ -0,0 +1,9 @@
+<?php
+class Home {
+    public function index() {
+        $data = [];
+
+        require_once 'views/home_view.php';
+    }
+}
+?>
---
+++ views/home_view.php
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Mock Layout</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+<body>
+<div class="container">
+
+    <section class="section">
+        <div class="circle"></div>
+        <div class="bar"></div>
+
+        <div class="small-box-row">
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+        </div>
+
+        <div class="large-box-row">
+            <div class="large-box"></div>
+            <div class="large-box"></div>
+            <div class="large-box"></div>
+            <div class="large-box"></div>
+        </div>
+    </section>
+
+
+    <section class="section">
+
+        <div class="circle"></div>
+        <div class="bar"></div>
+
+        <div class="small-box-row">
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+            <div class="small-box"></div>
+        </div>
+    </section>
+</div>
+</body>
+</html>
