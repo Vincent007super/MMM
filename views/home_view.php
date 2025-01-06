33fddeb46a265fdd2f8930d2d93e6e8ac3c1b59c
89329 <89329@glr.nl>
2025-01-06 14:13:00 +0100

    Update!

--- views/home_view.php
+++ views/home_view.php
@@ -4,46 +4,32 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Home Page</title>
-    <link rel="stylesheet" href="media/styles/site.css">
     <link rel="stylesheet" href="media/styles/home.css">  
 </head>
 <body>
 
 <?php include 'header.php' ?>
 
-    <div class="container">
-        <section class="section">
-            <div class="circle"></div>
-            <div class="bar"></div>
-            <div class="small-box-row">
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-            </div>
-            <div class="large-box-row">
-                <div class="large-box"></div>
-                <div class="large-box"></div>
-                <div class="large-box"></div>
-                <div class="large-box"></div>
-            </div>
-        </section>
+<main class="main-content">
+    <section class="section">
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+    </section>
 
-        <section class="section">
-            <div class="circle"></div>
-            <div class="bar"></div>
-            <div class="small-box-row">
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-                <div class="small-box"></div>
-            </div>
-        </section>
-    </div>
+    <section class="section">
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+    </section>
+
+    <section class="section">
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+      <div class="content-block"></div>
+    </section>
+  </main>
 </body>
 </html>
