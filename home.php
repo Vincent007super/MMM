a39bde56ff8ee54094942e5d87db409c06e81bbc
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2025-01-13 00:56:02 +0100

    added dynamic loading of home.php content
    W.I.P
    
    The connection to the DB is to be tested and changed on the server

--- home.php
+++ home.php
@@ -1,4 +1,45 @@
 <?php
+// include 'config.php'; // Your database connection file
+
+// // Connect to the database
+// $conn = new mysqli($host, $user, $pass, $db);
+
+// if ($conn->connect_error) {
+//     die("Connection failed: " . $conn->connect_error);
+// }
+
+// // Fetch all artists
+// $artists = $conn->query("SELECT * FROM artists");
+
+// // Function to fetch random songs for an artist
+// function getSongsByArtist($conn, $artist_id, $limit = 6) {
+//     $sql = "SELECT * FROM songs WHERE artist_id = $artist_id LIMIT $limit";
+//     return $conn->query($sql);
+// }
+
+// // Function to fetch random songs
+// function getRandomSongs($conn, $limit = 4) {
+//     $sql = "SELECT songs.*, artists.name AS artist_name 
+//             FROM songs 
+//             JOIN artists ON songs.artist_id = artists.id 
+//             ORDER BY RAND() LIMIT $limit";
+//     return $conn->query($sql);
+// }
+
+// // Prepare the data for the page
+// $artist_rows = [];
+// while ($artist = $artists->fetch_assoc()) {
+//     $songs = getSongsByArtist($conn, $artist['id']);
+//     $artist_rows[] = [
+//         'artist' => $artist,
+//         'songs' => $songs->fetch_all(MYSQLI_ASSOC),
+//     ];
+// }
+
+// // Fetch random songs
+// $random_songs = getRandomSongs($conn);
+
+// // Close connection
+// $conn->close();
 
 include 'views/home_view.php';
-
