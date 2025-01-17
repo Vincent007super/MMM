5a92ee97cf28402173fc3ec54039f3f641434f3a
Vincent007super <115561924+Vincent007super@users.noreply.github.com>
2025-01-17 16:03:41 +0100

    Updated home.php
    - Added animations to home.php
    - Updated home.css

--- views/home_view.php
+++ views/home_view.php
@@ -29,12 +29,11 @@
                 <div class="wrapper2">
                     <?php foreach ($row['nummers'] as $nummer): ?>
                         <div class="wrapper3">
-                            <div class="nummerImg" style="background-image: url('media/images/nummers/<?= $nummer['image_dataN'] ?>');"></div>
+                            <div class="nummerImg"
+                                style="background-image: url('media/images/nummers/<?= $nummer['image_dataN'] ?>');"></div>
                             <div class="wrapper4">
                                 <h1><?= htmlspecialchars($nummer['Sname']) ?></h1>
-                                <div class="wrapper5">
-                                    <h5><?= htmlspecialchars($row['Artiest']['artname']) ?></h5>
-                                </div>
+                                <h5><?= htmlspecialchars($row['Artiest']['artname']) ?></h5>
                             </div>
                         </div>
                     <?php endforeach; ?>
@@ -46,7 +45,8 @@
             <div class="wrapper2">
                 <?php foreach ($random_nummers as $nummer): ?>
                     <div class="wrapper3">
-                        <div class="nummerImg" style="background-image: url('media/images/nummers/<?= $nummer['image_dataN'] ?>');"></div>
+                        <div class="nummerImg"
+                            style="background-image: url('media/images/nummers/<?= $nummer['image_dataN'] ?>');"></div>
                         <div class="wrapper4">
                             <h1><?= htmlspecialchars($nummer['Sname']) ?></h1>
                             <div class="wrapper5">
